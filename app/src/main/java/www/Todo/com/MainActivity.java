e92e81d9d7d09c49cea6aa308f4717a774382382
Amrutha <amruthajayaram19@gmail.com>
2017-10-18 11:12:51 +0530

    Initial commit

---
+++ app/src/main/java/www/Todo/com/MainActivity.java
@@ -0,0 +1,51 @@
+package www.Todo.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /*
+        * Declared EditText Objects  and called as todoTitleEdittext,todoDateEditText,todoDescriptionEditText;
+        * Declared Button Object and called as saveTodoButton;
+     */
+    EditText todoTitleEditText, todoDateEditText, todoDescriptionEditText;
+    Button saveTodoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Inflate layout using setContentView Method
+        setContentView(R.layout.activity_main);
+        initObjects();
+        initCallBacks();
+
+    }
+
+    /*
+    In this method initialized all the editText objects
+     */
+    public void initObjects() {
+        /*
+         using findViewById references to the todoTitleEdittext,todoDateEditText,todoDescriptionEditText,saveTodoButton to  from layout
+         */
+        todoTitleEditText = (EditText) findViewById(R.id.editText_todo_title);
+        todoDateEditText = (EditText) findViewById(R.id.editText_todo_date);
+        todoDescriptionEditText = (EditText) findViewById(R.id.editText_todo_description);
+        saveTodoButton = (Button) findViewById(R.id.button_save_todo);
+    }
+
+    /*
+    In this method registered event callbacks
+     */
+    public void initCallBacks() {
+        saveTodoButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
